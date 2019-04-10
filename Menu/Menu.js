36190a20e9ac530602b5279f1539b150558311a3
Ming Liu <mliu@macpro.home>
2019-04-10 15:16:49 -0400

    commit before trying out something that has the potential to be very buggy

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -12,7 +12,18 @@ const menuButton = document.querySelector(".menu-button");
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", (event) => {
+  event.stopPropagation();
   toggleMenu();
 })
 
-
+let allElements = document.querySelectorAll("div");
+allElements.forEach(elem => {
+  if(!elem.classList.contains("menu")){
+    elem.addEventListener("click", (event) => {
+          if(menu.classList.contains("menu--open")) {
+            menu.classList.toggle("menu--open");
+          }
+        })
+    }
+})
+  
